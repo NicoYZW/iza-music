e8eff92c6332d8fefbfc5a5b2b471299ef5b5d3c
NicoYZW <67188910+NicoYZW@users.noreply.github.com>
2021-03-26 15:18:30 +0100

    Initials files

---
+++ config/bot.js
@@ -0,0 +1,17 @@
+module.exports = {
+    emojis: {
+        off: ':x:',
+        error: ':warning:',
+        queue: ':bar_chart:',
+        music: ':musical_note:',
+        success: ':white_check_mark:',
+    },
+
+    discord: {
+        token: 'ODI0OTkxODM4MTQ5ODA0MDQz.YF3bhQ.2mgdnJYsIvHxpyEojeL3ateuMIY',
+        prefix: '#',
+        activity: 'IZΛ MUSIQUE',
+    },
+
+    filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+};
